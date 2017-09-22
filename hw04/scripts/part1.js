f7b39fc59f864e2cd367f85d8a8e6d1f41a676da
Morgan <mo.aspen.sierra@gmail.com>
2017-09-21 21:06:01 -0700

    My First Commit

--- hw04/scripts/part1.js
+++ hw04/scripts/part1.js
@@ -4,12 +4,14 @@ let languageCode
 const setLanguage = (code) => {
     //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
     languageCode = code
-    if (code === 'ru') {
-        language = 'Russian'
-    } else if (code === 'es') {
-        language = 'Spanish'
+    if (code === 'az') {
+        language = 'Azerbaijani'
+    } else if (code === 'bn') {
+        language = 'Bengali'
     } else {
-        language = 'English'
+        language = 'Basque'
     }
     document.getElementById('language').innerHTML = language
 }
+// I think the set language is a javascript function that we've set to change the language.
+// Another thing we do is change the wording.
