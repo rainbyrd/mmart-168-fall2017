cf847dcd4a54f11108b008035650beb8e14adeb3
Morgan <mo.aspen.sierra@gmail.com>
2017-10-18 17:16:35 -0700

    Complete Homework 7

--- hw07/js/index.js
+++ hw07/js/index.js
@@ -7,7 +7,9 @@
 // appends an "li" element, with the string inside, to the un-ordered
 // list ("ul" tag):
 
-
+const addListItemToUnorderedHTMLList = (message) => {
+  document.getElementById('output').innerhtml += "<li>" + message + " </li>"
+}
 
 //------------------------------------------------------------------
 // PART II
@@ -19,3 +21,16 @@
 // 2. If the number is divisible by 5, append "Buzz" to the unordered list
 // 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
 // 4. Bonus points: Include both the number *and* the word you've appended to the list
+
+let start = 0
+
+for (let i = 1; i <= 100 i++) {
+  if (i % 3 === 0) {
+    addListItemToUnorderedHTMLList("fizz")
+  } else if (i % 5 ===0) {
+    addListItemToUnorderedHTMLList("Buzz")
+  } else if (i % 3 === 0 && i % 5 === 0)
+  addListItemToUnorderedHTMLList("FizzBuzz")
+  } else {
+    addListItemToUnorderedHTMLList(i)}
+}
